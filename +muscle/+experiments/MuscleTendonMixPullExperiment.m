995ece26f66cefeb779d062229b6640168351bbb
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2015-12-15 17:54:59 +0100

    moved experiment modelconfigs to muscle model (has nothing to do with full muscle model)

---
+++ +muscle/+experiments/MuscleTendonMixPullExperiment.m
@@ -0,0 +1,293 @@
+classdef MuscleTendonMixPullExperiment < models.muscle.AExperimentModelConfig
+    % A simple experiment that pulls at one side with constant force but
+    % varying muscle-tendon ratio at the fixed side.
+    
+    properties
+        Variant;
+    end
+    
+    methods
+        function this = MuscleTendonMixPullExperiment(varargin)
+            this = this@models.muscle.AExperimentModelConfig(varargin{:});
+            this.init;
+            
+            this.NumConfigurations = 10;
+            this.NumOutputs = 20;
+        end
+        
+        function configureModel(this, m)
+            configureModel@models.muscle.AExperimentModelConfig(this, m);
+            m.T = 99;
+            m.dt = 1;
+            m.DefaultInput = 1;
+            
+            os = m.ODESolver;
+            os.RelTol = 1e-5;
+            os.AbsTol = 1e-5;
+            
+            mu = m.DefaultMu;
+            mu(2) = 0;
+            mu(3) = .1;
+            m.DefaultMu = mu;
+        end
+        
+        function configureModelFinal(this)
+            this.Model.Plotter.GeoView = [32 40];
+        end
+        
+        function u = getInputs(this)
+            % Ramp up the external pressure
+            u{1} = this.getAlphaRamp(80,1);
+        end
+        
+        function tmr = getTendonMuscleRatio(this, points)
+            % Returns the [0,1] ratio between tendon and muscle at all
+            % specified points
+            %
+            % This method simply returns an all-zero ratio, meaning muscle only. 
+            tmr = zeros(1,size(points,2));
+            ratio = (this.CurrentConfigNr-1)/(this.NumConfigurations-1);
+            tmr(points(1,:)<3) = ratio;
+        end
+        
+        function P = getBoundaryPressure(this, elemidx, faceidx)
+            % Determines the neumann forces on the boundary.
+            %
+            % The unit for the applied quantities is megaPascal [MPa]
+            %
+            % In the default implementation there are no force boundary
+            % conditions.
+            P = [];
+            if this.Options.GeoNr == 3
+                if any(elemidx == 28:36) && faceidx == 2
+                    P = 1;
+                end
+            else
+                if elemidx == this.Options.GeoNr && faceidx == 2
+                    P = 1;
+                end
+            end
+        end
+        
+        function o = getOutputOfInterest(this, ~, y)
+            m = this.Model;
+            if this.Options.GeoNr == 3
+                facedof = m.getFaceDofsGlobal(28:36,2,1);
+            else
+                facedof = m.getFaceDofsGlobal(this.Options.GeoNr,2,1);
+            end
+            y = m.System.includeDirichletValues(m.Times,y);
+            % Get x position of one node at end as output
+            o = NaN(1,this.NumOutputs);
+            idx = floor(linspace(1,m.T,this.NumOutputs));
+            hasdata = idx <= size(y,2);
+            o(hasdata) = mean(y(facedof,idx(hasdata)),1);
+        end
+    end
+    
+    methods(Access=protected)
+        
+        function geo = getGeometry(this)
+            switch this.Options.GeoNr
+                case 1
+                    % A single cube
+                    geo = fem.geometry.RegularHex8Grid([0 10],[0 10],[0 10]);
+                case 2
+                    % two cubes split at 3
+                    geo = fem.geometry.RegularHex8Grid([0 3 10],[0 10],[0 10]);
+                case 3
+                    % sixteen cubes
+                    geo = fem.geometry.RegularHex8Grid(...
+                        [0 1.5 3 6.5 10],0:3.33333:10,0:3.33333:10);
+            end
+            geo = geo.toCube27Node;    
+        end
+        
+        function displ_dir = setPositionDirichletBC(this, displ_dir)
+            %% Dirichlet conditions: Position (fix one side)
+            geo = this.FEM.Geometry;
+            if this.Options.GeoNr == 3
+                displ_dir(1,geo.Elements(1:9,geo.MasterFaces(1,:))) = true;
+            else
+                displ_dir(1,geo.Elements(1,geo.MasterFaces(1,:))) = true;
+            end
+        end
+        
+        function anull = seta0(~, anull)
+            anull(1,:,:) = 1;
+        end
+    end
+    
+    methods(Static)
+        function RunMuscleTendonMixPullExperiment(geo)
+            if nargin < 1
+                geo = 1;
+            end
+            
+            %% Init
+            c = models.fullmuscle.experiments.MuscleTendonMixPullExperiment('GeoNr',geo);
+            m = c.createModel;
+            e = models.muscle.ExperimentRunner(m);
+            if geo == 3
+                rules = [3 5];
+            else
+                rules = [3 4 5 7 10 15 20];
+            end
+            nrules = length(rules);
+            allo = zeros(c.NumConfigurations,c.NumOutputs,nrules);
+            ctimes = zeros(c.NumConfigurations,nrules);
+            
+            %% Computation
+            for gr = 1:nrules
+                grule = rules(gr);
+                m.setGaussIntegrationRule(grule);
+                [o, ctimes(:,gr)] = e.runExperiment;
+                allo(:,:,gr) = o;
+            end
+            %% Save
+            save(fullfile(c.OutputDir,['output_' c.getOptionStr(false) '.mat']),'allo','ctimes','rules');
+            %% Load
+            load(fullfile(c.OutputDir,['output_' c.getOptionStr(false) '.mat']));
+            
+            %% per-gauss-rule comparison
+%             pm = PlotManager(false,1,2);
+            pm = PlotManager;
+            pm.ExportDPI = 200;
+            pm.LeaveOpen = true;
+            pm.FilePrefix = sprintf('geo_%d',geo);
+            tmr = ((1:c.NumConfigurations)-1)/(c.NumConfigurations-1);
+            
+            %% Plot results
+            [TMR,T] = meshgrid(tmr,...
+                    floor(linspace(1,m.T,c.NumOutputs))); 
+            pt = PrintTable;
+            pt.Caption = sprintf('Average x-Position of right face errors, config %s',c.getOptionStr);
+            pt.HasHeader = true;
+            pt.addRow('Gauss rule','Max absolute','Mean absolute','Max relative','Mean relative');
+            for gr = 1:nrules
+                grule = rules(gr);
+                ax = pm.nextPlot(sprintf('gaussrule_%d',grule),...
+                    sprintf('Right face position over time and tm-ratio, Gauss %d-point rule',grule),...
+                    'tendon-muscle ratio \theta_i','time [ms]');
+                surfc(TMR,T,allo(:,:,gr)','Parent',ax,'FaceColor','interp');
+                view([32 38]);
+                ax = pm.nextPlot(sprintf('gaussrule_%d_err',grule),...
+                    sprintf('Gauss %d-point rule error relative to %d-point rule',grule,rules(end)),...
+                    'tendon-muscle ratio \theta_i','time [ms]');
+                abserr = abs(allo(:,:,gr)-allo(:,:,end));
+                relerr = abs((allo(:,:,gr)-allo(:,:,end)))./allo(:,:,end);
+                mesh(TMR,T,relerr','Parent',ax);
+                pt.addRow(grule,max(abserr(:)),max(relerr(:)),mean(abserr(:)),mean(relerr(:)),...
+                    {'%d-point','%g','%g','%g','%g'});
+            end
+            pt.print;
+            
+            %% Save images
+            pt.Format = 'tex';
+            pt.saveToFile(fullfile(c.OutputDir,['output_' c.getOptionStr(false) '.tex']));
+            pm.SaveFormats = {'jpg','png'};
+            if geo == 2
+                pm.savePlots(c.ImgDir,'Selection',11);
+            else
+                pm.savePlots(c.ImgDir,'Selection',2:2:12,'Format','pdf');
+                pm.savePlots(c.ImgDir,'Selection',1:2:11);
+            end
+            pm.closeAll;
+            
+            %% End position plot
+            ax = pm.nextPlot('endpos',...
+                'Comparison of face positions over TMR and gauss rules',...
+                'tendon-muscle ratio \theta_i','gauss integration rule [points]');
+            [TMR,GR] = meshgrid(tmr,1:nrules);
+            Z = squeeze(allo(:,end,:))';
+            surfc(TMR,GR,Z,'Parent',ax,'FaceColor','interp');
+            pm.done;
+            set(ax,'YTickLabel',sprintfc('%d',rules),'YTick',1:nrules);
+            zlabel('Mean x-position')
+            view([34 60]);
+            pm.copyFigure(1);
+            view([0 90]);
+            pm.savePlots(c.ImgDir,'Close',true);
+            ax = pm.nextPlot('endpos_2d',...
+                'Comparison of face positions over TMR',...
+                'tendon-muscle ratio \theta_i');
+            plot(ax,tmr,Z);
+            legend(sprintfc('%d-point',rules));
+            pm.savePlots(c.ImgDir,'Format','pdf');
+            pm.closeAll;
+            
+            %% Geo setup plots
+            pm.NoTitlesOnSave = true;
+            conf = [1 5 10];
+            if geo == 3
+                rule = rules;
+            else
+                rule = [3 7 20];
+            end
+            for k = 1:3
+                for r = 1:3
+                    m.setGaussIntegrationRule(rule(r));
+                    c.CurrentConfigNr = conf(k);
+                    m.Plotter.GeoView = [32 40];
+                    m.plotGeometrySetup(pm);
+                    pm.FilePrefix = sprintf('geo_%d_conf_%d_gauss_%d',geo,conf(k),rule(r));
+                    pm.savePlots(c.ImgDir,'Format','jpg');
+                end
+            end
+            pm.closeAll;
+        end
+        
+        function NonGeoDependentPlots
+            pm = PlotManager;
+            pm.ExportDPI = 200;
+            pm.LeaveOpen = true;
+            c = experiments.MuscleTendonMixPullExperiment;
+            m = c.createModel;
+            
+            %% Functions
+            mu = m.DefaultMu;
+            mlfg = tools.MarkertLawOriginal(mu(5),mu(6));
+            mlfg.plot('PM',pm,'R',[.9 1.3]);
+            % Tendon anisotropic passive law
+            mlfg = tools.CubicToLinear(mu(7),mu(8));
+            mlfg.xLabel = '\lambda [-]';
+            mlfg.yLabel = 'Pressure [MPa]';
+            mlfg.plot('PM',pm,'R',[.99 1.04]);
+            pm.FilePrefix = 'aniso';
+            pm.savePlots(c.ImgDir,'Format','pdf','Selection',[1 3]);
+            pm.closeAll;
+            
+            %% Comparison plots
+            c = experiments.MuscleTendonMixPullExperiment('GeoNr',1);
+            load(fullfile(c.OutputDir,['output_' c.getOptionStr(false) '.mat']));
+            allo_g1 = allo;
+            c = experiments.MuscleTendonMixPullExperiment('GeoNr',2);
+            load(fullfile(c.OutputDir,['output_' c.getOptionStr(false) '.mat']));
+            nrules = length(rules);
+            allo_g2 = allo;
+            abserr = abs(allo_g1-allo_g2);
+            relerr = abs((allo_g1-allo_g2)./allo_g2);
+            tmr = ((1:c.NumConfigurations)-1)/(c.NumConfigurations-1);
+            [TMR,GR] = meshgrid(tmr,1:nrules);
+            ax = pm.nextPlot('endpos_abserr',...
+                    sprintf('Right face position over time and tm-ratio, Gauss %d-point rule',1),...
+                    'tendon-muscle ratio \theta_i','time [ms]');
+            Z = squeeze(abserr(:,end,:))';
+            surfc(TMR,GR,Z,'Parent',ax,'FaceColor','interp');
+            set(ax,'YTickLabel',sprintfc('%d',rules),'YTick',1:nrules);
+            zlabel('absolute x-position error');
+            ax = pm.nextPlot('endpos_relerr',...
+                    sprintf('Right face position over time and tm-ratio, Gauss %d-point rule',1),...
+                    'tendon-muscle ratio \theta_i','time [ms]');
+            
+            Z = squeeze(relerr(:,end,:))';
+            surfc(TMR,GR,Z,'Parent',ax,'FaceColor','interp');
+            set(ax,'YTickLabel',sprintfc('%d',rules),'YTick',1:nrules);
+            zlabel('% relative x-position error');
+            pm.done;
+            pm.savePlots(c.ImgDir,'Close',true);
+        end
+    end
+    
+end
+
---
+++ +muscle/+experiments/QuickRelease.m
@@ -0,0 +1,358 @@
+classdef QuickRelease < models.muscle.AExperimentModelConfig
+% Provides a model config and test scripts for a quick release test.
+%
+% The material parameters used are suggested by Thomas Heidlauf, see his
+% thesis.
+  
+    properties(SetAccess=private)
+        % The time the velocity BCs are applied in order to reach the
+        % initial condition
+        icMovetime = 100; % [ms]
+        
+        % The time used to increase the muscle activation
+        icAlphaRampTime = 100; % [ms]
+        
+        % The time after movement and activation is fully done until the
+        % ICs are extracted ("quasi static")
+        icRelaxTime = 100000; % [ms]
+        
+        Loads;
+        Pressures;
+    end
+
+    methods
+        function this = QuickRelease(varargin)
+            this = this@models.muscle.AExperimentModelConfig(varargin{:});
+            this.init;
+        end
+        
+        function configureModel(this, m)
+            configureModel@models.muscle.AExperimentModelConfig(this, m);
+            os = m.ODESolver;
+            switch this.GeoNr
+                case 1
+                    if this.ICCompMode
+                        os.RelTol = 1e-7;
+                        os.AbsTol = .009;
+                    else
+                        os.RelTol = .01;
+                        os.AbsTol = .1;
+                    end
+                case 2
+                    if this.ICCompMode
+                        os.RelTol = 1e-7;
+                        os.AbsTol = .009;
+                    else
+                        os.RelTol = .01;
+                        os.AbsTol = .1;
+                    end
+                case 3
+                    if this.ICCompMode
+                        os.RelTol = 1e-7;
+                        os.AbsTol = .02;
+                    else
+                        os.RelTol = .01;
+                        os.AbsTol = .8;
+                    end
+            end
+            %% Material setup
+            % Material set (see main comment)
+            m.DefaultMu(5) = 0.00355439810963035; % b1 [MPa]
+            m.DefaultMu(6) = 12.660539325481963; % d1 [-]
+            m.DefaultMu(9) = 6.352e-10; % c10 [MPa]
+            m.DefaultMu(10) = 3.627; % c01 [MPa]
+            m.DefaultMu(13) = .250; % [MPa]
+            m.DefaultMu(14) = 1.2; % [-]
+            
+            if this.ICCompMode
+                % No activation needed for position/IC computing
+                m.DefaultMu(2) = 0;
+%                 f.alpha = this.getAlphaRamp(this.icAlphaRampTime, alpha);
+                m.T = max(this.icMovetime, this.icAlphaRampTime) + this.icRelaxTime;
+                m.dt = m.T / 300;
+                this.VelocityBCTimeFun = tools.ConstantUntil(this.icMovetime);
+            else
+                m.T = 20;
+                m.dt = .1;
+                m.DefaultMu(2) = 2*m.dt;
+                m.EnableTrajectoryCaching = true;
+            end
+        end
+        
+        function x0 = getX0(this, x0)
+            if ~this.ICCompMode
+                s = load(fullfile(QuickRelease.OutputDir,sprintf('geo%d_ic.mat',this.GeoNr)));
+                x0 = s.x0;
+            end
+        end
+        
+        function o = getOutputOfInterest(this, m, t, uvw)
+            geo = m.Config.PosFE.Geometry;
+            [df,nf] = m.getResidualForces(t,uvw);
+            uvw = m.System.includeDirichletValues(t, uvw);
+            switch this.GeoNr
+                case 1
+                    % Get average velocity of loose end
+                    facenode_idx = m.getFaceDofsGlobal(2,2,1);
+                    o(1,:) = mean(uvw(facenode_idx+geo.NumNodes*3,:),1);
+                    
+                    % Get force on that end [N]
+                    o(2,:) = abs(sum(nf,1))/1000;
+                    % Also store the dirichlet forces (maybe we'll need it)
+                    % [N]
+                    o(3,:) = sum(df,1)/1000;
+                case 2
+                    facenode_idx = [];
+                    for k = 1:4
+                        facenode_idx = [facenode_idx; m.getFaceDofsGlobal(k,3,2)];%#ok
+                    end
+                    % Save some work
+                    facenode_idx = unique(facenode_idx);
+                    o(1,:) = mean(uvw(facenode_idx+geo.NumNodes*3,:),1);
+                    
+                    % Get force on that end [N]
+                    o(2,:) = abs(sum(nf,1))/1000;
+                    % Also store the dirichlet forces (maybe we'll need it)
+                    % [N]
+                    o(3,:) = sum(df,1)/1000;
+                case 3
+                    % Get average velocity of loose end
+                    facenode_idx = m.getFaceDofsGlobal(6,3,2);
+                    o(1,:) = mean(uvw(facenode_idx+geo.NumNodes*3,:),1);
+                    
+                    % Get force on that end [N]
+                    o(2,:) = abs(sum(nf,1))/1000;
+                    % Also store the dirichlet forces (maybe we'll need it)
+                    % [N]
+                    o(3,:) = sum(df,1)/1000;
+            end
+        end
+        
+        function P = getBoundaryPressure(this, elemidx, faceidx)
+            % Determines the neumann forces on the boundary.
+            %
+            % The unit for the applied quantities is kiloPascal [MPa]
+            %
+            % In the default implementation there are no force boundary
+            % conditions.
+            P = [];
+            if ~this.ICCompMode
+                switch this.GeoNr
+                    case 1
+                        if elemidx == 2 && faceidx == 2
+                            P = 1;
+                        end
+                    case 2
+                        if elemidx <= 4 && faceidx == 3
+                            P = 1;
+                        end
+                    case 3
+                        if elemidx == 6 && faceidx == 3
+                            P = 1;
+                        end
+                end
+            end
+        end
+        
+        function u = getInputs(this)
+            % loads in [g]
+            loads = [0 100 2000];
+
+            switch this.GeoNr
+                case 1
+                    a = this.PosFE.getFaceArea(2,2);
+                case 2
+                    a = this.PosFE.getFaceArea(4,3);
+                case 3
+                    a = this.PosFE.getFaceArea(6,3);
+            end
+            m = this.Model;
+            % [g*mm/ms^2/mm^-2] = [N/mm^-2] = [MPa]
+            pressures = loads*m.Gravity/a; % [MPa]
+            u = {};
+            for lidx = 1:length(loads)
+                pressure = pressures(lidx);
+                u{lidx} = this.getAlphaRamp(2*m.dt,pressure);%#ok
+            end
+            this.Loads = loads;
+            this.Pressures = pressures;
+        end
+        
+    end
+    
+    methods(Access=protected)
+        
+        function geo = getGeometry(this)
+            switch this.Options.GeoNr
+                case 1
+                    geo = fem.geometry.RegularHex8Grid(0:20:40,[0 20],[0 20]);
+                case 2
+                    geo = fem.geometry.Belly(3, 50, 'Radius', 4.5, 'InnerRadius', 1.5, 'Gamma', 15);
+                case 3
+                    s = load(fullfile(fileparts(which(mfilename)),'EntireTA.mat'));
+                    geo = s.geo27;
+            end
+        end
+        
+        function displ_dir = setPositionDirichletBC(this, displ_dir)
+            
+            geo = this.PosFE.Geometry;
+            %% Dirichlet conditions: Position (fix one side)
+            % This is done for each test case.
+            switch this.GeoNr
+                case 1
+                    % Fix front
+                    face = geo.MasterFaces(1,:);
+                    displ_dir(:,geo.Elements(1,face(5))) = true;
+                    displ_dir(1,geo.Elements(1,face([1:4 6:9]))) = true;
+                case 2
+                    % Fix back side
+                    for k = geo.NumElements-3:geo.NumElements
+                        displ_dir(:,geo.Elements(k,geo.MasterFaces(4,:))) = true;
+                    end
+                case 3
+                    % Fix broad end of TA
+                    displ_dir(:,geo.Elements(8,geo.MasterFaces(4,:))) = true;
+                    displ_dir(:,geo.Elements(8,geo.MasterFaces(2,:))) = true;
+                    % For quick release test we also constrain the movement
+                    % of the loose end to the y axis
+                    if ~this.ICCompMode
+                        displ_dir([1 3],geo.Elements(6,geo.MasterFaces(3,:))) = true;
+                    end
+            end
+        end
+        
+        function [velo_dir, velo_dir_val] = setVelocityDirichletBC(this, velo_dir, velo_dir_val)
+            % Determines the dirichlet velocities.
+            %
+            % The unit for the applied quantities is [mm/ms] = [m/s]
+            %
+            % These conditions are required to compute the initial
+            % conditions for the quick release test.
+            if this.ICCompMode
+                geo = this.PosFE.Geometry;
+                box = geo.getBoundingBox;
+                f = this.Model.System.f;
+                
+                switch this.GeoNr
+                    case 1
+                        len = box(2)-box(1);
+                        velo_dir(1,geo.Elements(2,geo.MasterFaces(2,:))) = true;
+                        totaldistance = (f.lambdafopt-1)*len;
+                        velo_dir_val(velo_dir) = totaldistance/this.icMovetime;
+                    case 2
+                        for k = 1:4
+                           velo_dir(2,geo.Elements(k,geo.MasterFaces(3,:))) = true;
+                        end
+                        totaldistance = (f.lambdafopt-1)*(box(4)-box(3));
+                        velo_dir_val(velo_dir) = -totaldistance/this.icMovetime;
+                    case 3
+                        len = box(4)-box(3);
+                        % Set only for y direction to nonzero value
+                        velo_dir(2,geo.Elements(6,geo.MasterFaces(3,:))) = true;
+                        totaldistance = (f.lambdafopt-1)*len;
+                        velo_dir_val(velo_dir) = -totaldistance/this.icMovetime;
+                        % Set zero for rest
+                        velo_dir(:,geo.Elements(6,geo.MasterFaces(3,:))) = true;
+                end
+            end
+        end
+        
+        function anull = seta0(this, anull)
+            switch this.GeoNr
+                case {1, 3}
+                    % Fibres in x direction
+                    anull(1,:,:) = 1;
+                case 2
+                    % Fibres in y direction
+                    anull(2,:,:) = 1;
+            end
+        end
+    end
+    
+    methods(Static)
+        function runTest(geonr)
+            if nargin < 1
+                geonr = 1;
+            end
+            
+            %% Initial conditions
+            x0file = fullfile(QuickRelease.OutputDir,sprintf('geo%d_ic.mat',geonr));
+            if exist(x0file,'file') ~= 2
+                mc = QuickRelease(geonr, true);
+                m = models.muscle.Model(mc);
+                mu = m.DefaultMu;
+                mu(1) = .001;
+                [t, y] = m.simulate(mu);
+%                 m.plot(t,y);
+                yfull = m.System.includeDirichletValues(t,y);
+                x0 = yfull(:,end);%#ok
+                save(x0file,'x0','t','y','m');
+                fprintf('Initial conditions file created. Please re-run.\n');
+                return;
+            end
+            
+            file = fullfile(QuickRelease.OutputDir,sprintf('geo%d.mat',geonr));
+            saveit = true;
+            if exist(file,'file') == 2
+                load(file);
+                saveit = false;
+            else
+                mc = QuickRelease(geonr, false);
+                m = models.muscle.Model(mc);
+            end
+%             geo = mc.PosFE.Geometry;
+            
+            pm = PlotManager(false,2,2);
+%             pm = PlotManager;
+            pm.AutoTickMarks = 0;
+            pm.LeaveOpen = true;
+            
+            c = ColorMapCreator;
+            c.useJet([0.01 0.05 0.1 .5]);
+            
+            mus = repmat(m.DefaultMu,1,2);
+            mus(1,:) = [.1 1];
+            m.Data.ParamSamples = mus;
+            nparams = length(mus);
+            for k=1:nparams
+                mu = mus(:,k);
+                for inidx = 1:m.System.InputCount
+                    [t, y] = m.simulate(mu,inidx);
+                    o = m.Config.getOutputOfInterest(m, t, y);
+                    
+                    % Remove the first two entries as the activation is
+                    % increased there
+%                     o = o(:,3:end);
+
+                    if saveit
+                        save(file,'m','y','o','t','mc');
+                    end
+                    
+                    h = pm.nextPlot(sprintf('force_velo_load%g_visc%g',mc.Loads(inidx),mu(1)),...
+                        sprintf('Force / velocity plot\nLoad: %g[g] (eff. pressure %g[MPa]), viscosity:%g [mNs/m]',mc.Loads(inidx),mc.Pressures(inidx),mu(1)),...
+                        'Velocity [mm/ms]','Force [N]');
+                    plot(h,o(1,:),o(2,:),'r');
+                    
+%                     h = pm.nextPlot(sprintf('velo_visc%g',v),...
+%                         sprintf('Averaged X-velocity [mm/ms] of right face for different rates and activation level\nViscosity=%g',v),...
+%                         'Activation increase rate [1/ms]','alpha [-]');
+%                     surf(h,1./ramptimes, alphavals, abs(pos(:,:,2))','FaceColor','interp','EdgeColor',ec);
+%                     set(h,'XScale','log');
+%                     view(-136, 56);
+                end
+            end
+            
+%             m.plotGeometrySetup(pm);
+%             
+            pm.done;
+%             pm.FilePrefix = sprintf('case1_geo%d',geonr);
+%             pm.ExportDPI = 200;
+%             pm.SaveFormats = {'jpg'};
+%             pm.savePlots(QuasiStaticTest.OutputDir,'Close',true);
+        end
+       
+    end
+    
+end
+
